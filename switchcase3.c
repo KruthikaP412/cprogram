c77acb61e4f927882f05446370780d201ce9734b
kruthikashetty412 <56147881+kruthikashetty412@users.noreply.github.com>
2019-10-11 12:06:34 +0530

    Add files via upload

---
+++ switchcase3.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+int main()
+{
+	char x='a';
+	switch(x)
+	{
+		case 'b':
+			printf("Case b\n");
+			break;
+		case 'a':
+			printf("Case a\n");
+			break;
+		case 'c':
+			printf("Case c\n");
+			break;
+		default:
+			printf("NO MATCH\n");
+	}
+	return 0;
+}
+
