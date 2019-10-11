6b0be84e154511ab93527e65f51ebacdb7c64186
kruthikashetty412 <56147881+kruthikashetty412@users.noreply.github.com>
2019-10-11 11:23:17 +0530

    Add files via upload

---
+++ switchcase.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i;
+printf("enter value of i");
+scanf("%d",&i);
+switch(i)
+{
+case 1:
+      printf("case 1\n");
+      break;
+case 2:
+      printf("case 2\n");
+      break;
+case 3:
+      printf("case 3\n");
+      break;
+default:
+      printf("no case\n");
+} 
+}
