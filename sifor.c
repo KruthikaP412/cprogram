3a8d9b642bbe4f2329e52af9c53d4663206973d1
kruthikashetty412 <56147881+kruthikashetty412@users.noreply.github.com>
2019-10-15 10:14:07 +0530

    Add files via upload

---
+++ sifor.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+ 
+int main() 
+{
+float p, r, t, si;
+for(int i=0;i<3;i++)
+{
+printf("Enter principal,rate,time in year");   	
+scanf("%f %f %f",&p,&r,&t);
+si = (p*r*t)/100;
+printf("\n SI: %f \n",si);
+}
+return 0;
+}
+
